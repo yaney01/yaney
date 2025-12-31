52b420e9e09201cb13e15cdac0333cc76c1c4f71
yaney <53202722+yaney01@users.noreply.github.com>
2025-12-31 09:07:02 +0800

    Update script for WenZhenBaZiProCrack.js

--- WenZhenBaZiProCrack.js
+++ WenZhenBaZiProCrack.js
@@ -1,11 +1,29 @@
-[rewrite_local]
+/***********************************
 
-^https?:\/\/bzpp3\.iwzbz\.com\/api\/.*\/user\/getvipinfo url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/WenZhenBaZiProCrack.js
+> 应用名称：问真八字
+> 软件版本：1.2.2
+> 脚本作者：ddgksf2013
+> 微信账号：墨鱼手记
+> 解锁说明：解锁钻石会员权限
+> 更新时间：2023-08-04
+> 通知频道：https://t.me/ddgksf2021
+> 贡献投稿：https://t.me/ddgksf2013_bot
+> 问题反馈：ddgksf2013@163.com
+> 特别提醒：如需转载请注明出处，谢谢合作！
+> 特别说明：⚠️⚠️⚠️
+          本脚本仅供学习交流使用，禁止转载售卖
+          ⚠️⚠️⚠️
+
+
+[rewrite_local]
+  
+# > 问真八字☆解锁钻石会员权限（2023-08-04）@ddgksf2013
+^https?:\/\/bzpp2\.iwzbz\.com\/api\/.*\/user\/getvipinfo url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/WenZhenBaZiProCrack.js
 
 
 [mitm] 
 
-hostname=bzpp3.iwzbz.com
+hostname=bzpp2.iwzbz.com
 
 ***********************************/
 
