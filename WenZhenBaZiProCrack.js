b323a2795383177e69b48c7d93d935e5e3221dfa
yaney <53202722+yaney01@users.noreply.github.com>
2025-12-31 09:03:04 +0800

    Update hostname in WenZhenBaZiProCrack.js

---
+++ WenZhenBaZiProCrack.js
@@ -0,0 +1,37 @@
+[rewrite_local]
+  
+# > 问真八字☆解锁钻石会员权限（2023-08-04）@ddgksf2013
+^https?:\/\/bzpp3\.iwzbz\.com\/api\/.*\/user\/getvipinfo url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/WenZhenBaZiProCrack.js
+
+
+[mitm] 
+
+hostname=bzpp3.iwzbz.com
+
+***********************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var ddgksf2013={"code":-1,"data":{"vipTipsType":0,"vipLevel":3,"adtype":0,"expires":"2099-12-31 23:59:59"},"message":"","validateKey":"","isSuccess":true};
+$done({body:JSON.stringify(ddgksf2013)});
