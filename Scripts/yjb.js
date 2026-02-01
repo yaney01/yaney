8e3e7fc1c21245ce325b51eb5da28a11b55ae81d
yaney <53202722+yaney01@users.noreply.github.com>
2026-02-01 20:08:13 +0800

    Update yjb.js for VIP unlock functionality

---
+++ Scripts/yjb.js
@@ -0,0 +1,12 @@
+[rewrite_local]
+
+^https?:\/\/.*yangjibao\.com\/(?:wxapi\/)?account|vip_info url script-response-body https://github.com/yaney01/yaney/main/Scripts/yjb.js
+[mitm] 
+
+hostname = *.yangjibao.com
+
+
+var body = $response.body.replace(/vip_label":false/g, 'vip_label":true')
+						 .replace(/vip_expiry_date":null/g, 'vip_expiry_date":"2099-12-31"')
+						 .replace(/is_pay":false/g, 'is_pay":true');
+$done({ body });
